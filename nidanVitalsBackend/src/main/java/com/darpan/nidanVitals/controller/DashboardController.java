1170e3099e6e1d598476d34c0d3446cf3d7555c8
Darpan Pandey <darpanpandey2002@gmail.com>
2026-01-22 15:05:31 +0545

    feat: Created a Patient Model to store the FHIR Response and Configured the SQLite Database.

--- nidanVitalsBackend/src/main/java/com/darpan/nidanVitals/controller/DashboardController.java
+++ nidanVitalsBackend/src/main/java/com/darpan/nidanVitals/controller/DashboardController.java
@@ -1,12 +1,14 @@
 package com.darpan.nidanVitals.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/fhir")
+@CrossOrigin("http://localhost:3000")
 public class DashboardController {
 
 }
