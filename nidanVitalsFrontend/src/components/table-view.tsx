8b65af863690a757827ca4b958cc3cab36e8e504
Darpan Pandey <darpanpandey2002@gmail.com>
2026-01-23 19:12:03 +0545

    style(table-view): Added Badges for health status.

--- nidanVitalsFrontend/src/components/table-view.tsx
+++ nidanVitalsFrontend/src/components/table-view.tsx
@@ -1,5 +1,6 @@
 import { getStatusFromBmi } from "@/lib/fhir-utility";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
+import { Badge } from "./ui/badge";
 
 type PatientVital = {
     patientId: string;
@@ -10,6 +11,45 @@ type PatientVital = {
     diastolic: number | null;
 };
 const TableView = ({patients}: {patients: PatientVital[]}) => {
+
+    const renderBMIStatusBadge = (bmi: number) => {
+        switch (getStatusFromBmi(bmi).toLowerCase()) {
+            case "normal":
+                return (
+                    <Badge className="bg-green-100 text-green-800">
+                        Normal BMI
+                    </Badge>
+                );
+            case "overweight":
+                return (
+                    <Badge className="bg-yellow-100 text-yellow-800">
+                        Overweight
+                    </Badge>
+                );
+            case "obese":
+                return (
+                    <Badge className="bg-red-100 text-red-800">
+                        Obese
+                    </Badge>
+                );
+            case "underweight":
+                return (
+                    <Badge className="bg-blue-100 text-blue-800">
+                        Underweight
+                    </Badge>
+                );
+        }
+    };
+    const renderHypertensionStatusBadge = (systolic: number, diastolic: number) => {
+        if (systolic >= 140 || diastolic >= 90) {
+            return (
+                <Badge className="bg-red-100 text-red-800">
+                    Hypertension
+                </Badge>
+            );
+        };
+    }
+
     return(
         <Table>
             <TableCaption>Patient Vitals Records</TableCaption>
@@ -33,7 +73,15 @@ const TableView = ({patients}: {patients: PatientVital[]}) => {
                         <TableCell className="text-center">{vital.bmi}</TableCell>
                         <TableCell className="text-center">{vital.systolic}</TableCell>
                         <TableCell className="text-center">{vital.diastolic}</TableCell>
-                        <TableCell>{getStatusFromBmi(vital.bmi)}</TableCell>
+                        <TableCell>
+                            {
+                                renderBMIStatusBadge(vital.bmi || 0)
+                            }
+                            {'  '}
+                            {
+                                renderHypertensionStatusBadge(vital.systolic || 0, vital.diastolic || 0)
+                            }
+                        </TableCell>
                     </TableRow>
                 ))}
             </TableBody>
